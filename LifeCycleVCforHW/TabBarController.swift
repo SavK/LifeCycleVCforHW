90027d2371af580c6026f057778f3799bb84cb3b
Konstantin Savonevich <kossav@icloud.com>
2019-04-27 16:17:49 +0300

    customizing tabBar
    add icons, TabBarController. Work with collors

---
+++ LifeCycleVCforHW/TabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  TabBarController.swift
+//  LifeCycleVCforHW
+//
+//  Created by Savonevich Constantine on 4/27/19.
+//  Copyright © 2019 Savonevich Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    enum tabBarMenu: Int, CaseIterable {
+        case blue
+        case green
+        case yellow
+        case tomato
+        case purple
+    }
+    
+    override func viewDidLoad() {
+        
+        guard tabBar.items!.count == tabBarMenu.allCases.count else { return }
+        colorImagesForTabBar()
+        // set default color of first tabBarItem
+        self.tabBar.tintColor = view.backgroundColor
+    }
+    
+    // MARK: Private func for change image of tab bar item
+    private  func colorImagesForTabBar() {
+        
+        let tabBar = self.tabBar
+        
+        let blue = UIImage(named: "blue")?.withRenderingMode(.alwaysOriginal)
+        let green = UIImage(named: "green")?.withRenderingMode(.alwaysOriginal)
+        let yellow = UIImage(named: "yellow")?.withRenderingMode(.alwaysOriginal)
+        let tomato = UIImage(named: "tomato")?.withRenderingMode(.alwaysOriginal)
+        let purpure = UIImage(named: "purple")?.withRenderingMode(.alwaysOriginal)
+        
+        (tabBar.items![0] ).selectedImage = blue
+        (tabBar.items![1] ).selectedImage = green
+        (tabBar.items![2] ).selectedImage = yellow
+        (tabBar.items![3] ).selectedImage = tomato
+        (tabBar.items![4] ).selectedImage = purpure
+    }
+}
--- LifeCycleVCforHW/ViewController.swift
+++ LifeCycleVCforHW/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.tintColor = view.backgroundColor
+    }
 
 }
 
